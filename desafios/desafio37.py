c311c1f6d208905da44d6d1ec4bba0c2a2e78043
ravisousag <ravisousagarcindo@gmail.com>
2021-05-24 19:33:33 -0300

    desafio 37 curso em video

--- desafios/desafio37.py
+++ desafios/desafio37.py
@@ -1,3 +1,19 @@
-numero = float(input('Insira um numero: '))
-h = hex(numero)
-print(h)
+numero = int(input('Insira um numero: '))
+
+op = int(input('Escolha a base de conversão \n'
+               '1- Binário \n'
+               '2- octal \n'
+               '3- Hexadecimal'))
+if op == 1:
+    conv=bin(numero)
+    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+
+elif op == 2:
+    conv=oct(numero)
+    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+
+elif op == 3:
+    conv=hex(numero)
+    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+else:
+    print('Você escolheu opção inválida')
