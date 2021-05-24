38d5113fffb5196ae23d5f27bf1a4c5ec21c7ceb
ravisousag <ravisousagarcindo@gmail.com>
2021-05-24 19:44:00 -0300

    ajuste fateamento de string

--- desafios/desafio37.py
+++ desafios/desafio37.py
@@ -1,19 +1,16 @@
 numero = int(input('Insira um numero: '))
-
-op = int(input('Escolha a base de conversão \n'
-               '1- Binário \n'
-               '2- octal \n'
-               '3- Hexadecimal'))
+print ('''Escolha a base de conversão'
+               1- Binário
+               2- octal
+               3- Hexadecimal''')
+op = int(input('Sua opção: '))
 if op == 1:
-    conv=bin(numero)
-    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+    print('O número {} convertido em binániro é igual a: {}'.format(numero, bin(numero)[2:]))
 
 elif op == 2:
-    conv=oct(numero)
-    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+    print('O número {} convertido em octal é igual a: {}'.format(numero, oct(numero)[2:]))
 
 elif op == 3:
-    conv=hex(numero)
-    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, conv))
+    print('O número {} convertido em hexadecimal é igual a: {}'.format(numero, hex(numero)[2:]))
 else:
     print('Você escolheu opção inválida')
